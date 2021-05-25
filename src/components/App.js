b6e468ab20d33f8871e15c1a97c124f79e241f49
sid-bhatt <70953578+sid-bhatt@users.noreply.github.com>
2021-05-25 15:02:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super();
+    this.state = { time: new Date() };
+  }
+  componentDidMount() {
+    setInterval(this.currentTime, 1000);
+  }
+  componentDidUpdate() {
+    this.interval = setInterval(this.currentTime, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  currentTime = () => {
+    this.setState({ time: new Date() });
+  };
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="Clock">
+          <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
